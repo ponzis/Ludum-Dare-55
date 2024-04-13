2489b8bd8506abf87ccaa4fd36c98d4368c59165
Ponzis <ponzis.online@gmail.com>
2024-04-13 08:30:58 -0400

    Set up the first test scene

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -3,10 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var text = get_node('.').get_texture_normal()
-	var height = text.get_height()
-	var width = text.get_width()
-	pass # Replace with function  body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
