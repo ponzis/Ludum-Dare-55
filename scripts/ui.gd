c8b8c11ca348ffa59672ac34bfe56ae9e2a5fb45
Ponzis <ponzis.online@gmail.com>
2024-04-14 13:59:41 -0400

    Added item to display when clicked on

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -40,7 +40,45 @@ func show_dialoge():
 	var dialogue_node: Control = get_node_or_null('Dialogue')
 	if dialogue_node == null:
 		return
+	hide_display_image()
 	dialogue_node.show()
+
+
+func show_display_image():
+	var overlay_node: TextureButton = get_node_or_null('Overlay')
+	if overlay_node == null:
+		return
+	hide_dialoge()
+	overlay_node.show()
+
+func hide_display_image():
+	var overlay_node: TextureButton = get_node_or_null('Overlay')
+	if overlay_node == null:
+		return
+	overlay_node.hide()
+
+
+func make_clickmask(texture: Texture2D):
+	var clickmask: BitMap = BitMap.new()
+	if texture == null:
+		return
+		
+	var image = texture.get_image()	
+	clickmask.create_from_image_alpha(image)
+	return clickmask
+
+
+func display_image(overlay: Texture2D, description: String):
+	var overlay_node: TextureButton = get_node_or_null('Overlay')
+	var description_node: Label = get_node_or_null('Overlay/Description')
+	
+	overlay_node.set_texture_normal(overlay)
+	var clickmask = make_clickmask(overlay)
+	if clickmask != null:
+		overlay_node.set_click_mask(clickmask)
+	
+	description_node.set_text(description)
+	
 
 signal dialogue_clicked(option: int)
 
@@ -54,3 +92,8 @@ func _on_option_2_pressed():
 
 func _on_option_1_button_up():
 	dialogue_clicked.emit(0)
+
+signal overlay_clicked()
+
+func _on_overlay_pressed():
+	overlay_clicked.emit()
