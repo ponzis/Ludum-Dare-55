02c1b72e9a0f51d6b9a21921b27d36d8402e87cd
Lihp <lihp@localhost.localdomain>
2024-04-13 22:24:46 -0400

    Added pick up item class and added a function to handle setting flags to items.

---
+++ Rooms/HeadMaster/pickup_item.gd
@@ -0,0 +1,22 @@
+extends "res://scripts/item.gd"
+
+@export var required_pickup_flags := {}
+
+@export var set_flags_on_pickup := {}
+
+func _ready():
+	
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#Maybe add a replace node to it...
+
+func _on_pressed():
+	if check_conditions(required_pickup_flags):
+		set_flags(set_flags_on_pickup) # Set the relevant flags
+		self.hide() #Hide the picked up item
+	pass # Replace with function body.
