2f28e489ecf41bd5887a6c0b9ddbcfe06678f184
Ponzis <ponzis.online@gmail.com>
2024-04-14 00:29:47 -0400

    Added support for nested dialog and applying effects

--- scripts/dialog_action.gd
+++ scripts/dialog_action.gd
@@ -15,7 +15,22 @@ func _ready():
 
 func _on_dialogue_clicked(option):
 	print("Hit log ", option, " from ", dialogue['name'])
-	ui_manager.hide_dialoge()
+	
+	var resposnses = state['resposnses']
+	var selected_response = resposnses[option]
+	if selected_response.has('effect'):
+		var effect = selected_response['effect']
+		set_effect(effect)
+
+	if !selected_response.has('tree'):
+		ui_manager.hide_dialoge()
+		return
+		
+	set_dialogue_tree(selected_response['tree'])
+
+func set_effect(effect):
+	game_manager.game_flags[effect['key']] = effect['value']
+	print(game_manager.game_flags)
 
 func get_dialogue() -> Dictionary:
 	var file = FileAccess.open(dialog_file, FileAccess.READ)
@@ -34,7 +49,6 @@ func get_dialogue_tree(trees) -> Dictionary:
 	for tree in trees:
 		if not tree.has('condition'):
 			return tree
-		print(tree['condition'])
 		if check_codnition(tree['condition']):
 			return tree
 	return {}
@@ -45,7 +59,6 @@ func get_dialogue_responses(responses) -> Array:
 		if not response.has('condition'):
 			returns.append(response)
 			continue 
-		print(response['condition'])
 		if check_codnition(response['condition']):
 			returns.append(response)
 	return returns
@@ -56,19 +69,25 @@ func set_events():
 		ui_manager.dialogue_clicked.disconnect(conection['callable'])
 	ui_manager.dialogue_clicked.connect(_on_dialogue_clicked)
 
-func dialog():
-	print('Getting file')
-	#dialogue = get_dialogue()
-	ui_manager.show_dialoge()
-	set_events()
-	ui_manager.set_person_name(dialogue['name'])
-	var tree = get_dialogue_tree(dialogue['tree'])
+
+func set_dialogue_tree(dialogue_tree):
+	var tree = get_dialogue_tree(dialogue_tree)
+	state['tree'] = tree
+	
 	var resposnses = get_dialogue_responses(tree['response'])
+	state['resposnses'] = resposnses
 	
 	ui_manager.set_person_text(tree['text'])
 	ui_manager.set_person_resposnses(resposnses)
 
+func start_dialog():
+	set_events()
+	ui_manager.show_dialoge()
+	ui_manager.set_person_name(dialogue['name'])
+	set_dialogue_tree(dialogue['tree'])
+
+
 func _on_pressed():
 	print("START CHAT with %s" % dialogue['name'])
-	dialog()
+	start_dialog()
 	pass
